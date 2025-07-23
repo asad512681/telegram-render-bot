98368125f0bc6f2260cc2f311cdffdacb5c0ac57
asad512681 <asad512681@gmail.com>
2025-07-23 14:23:22 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
+BOT_TOKEN = os.getenv("8022680763:AAFT7do1jBGooaMJ7fRZNT8ujQV8sbVYxZI")
+MONGO_URI = os.getenv("mongodb+srv://asad512681:<db_password>@webdesignbot.z8xj5mx.mongodb.net/?retryWrites=true&w=majority&appName=webdesignbot")
